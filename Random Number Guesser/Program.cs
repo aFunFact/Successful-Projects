0802e503e698e385af0c97907ed86ec47f5d9164
Benjamin Nonesoung <nonesoungben@gmail.com>
2022-05-27 13:57:01 -0700

    Add project files.

---
+++ Random Number Guesser/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace RNG
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int rng = random.Next(100);
+            int ui = 0;
+            int i = 0;
+            while (true)
+            {
+                if (rng != ui)
+                {
+                    Console.Write("Guess: ");
+                    string user = Console.ReadLine();
+                    ui = Convert.ToInt32(user);
+                    i++;
+                    Console.WriteLine($"Tries: {i}");
+
+                    if (ui < rng)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                  else if (ui > rng)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Tries: {i}");
+                        i++;
+                        
+                    }
+                    
+                }
+                else if (rng == ui)
+                {
+                    Console.WriteLine("Congrats you Got it Right");
+                    break;
+                }
+            }
+        }
+    }
+}
